6c2f3dc16b09a0d4c0cf6df80e263e8b5140ef04
udhayasudharsanan K <140906893+udhayasudharsanan@users.noreply.github.com>
2024-11-15 17:36:52 +0530

    COD and DELIVERY

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -25,7 +25,7 @@ export default function Cart() {
   const navigate = useNavigate();
   const token = localStorage.getItem('token'); // Retrieve the token from localStorage
   const API_URL = process.env.REACT_APP_BACKEND_URL || 'https://us-farmconnect.onrender.com';
-  const customerId = localStorage.getItem('customerId'); // or from context
+  const customerId = localStorage.getItem('user.userId'); // or from context
 
   // Fetch updated cart data after login or negotiation (if needed)
   
@@ -115,7 +115,7 @@ const handleCheckout = async () => {
       alert('Please enter an address.');
       return;
     }
-    const customerId = user.userId;
+    
 
     if (!customerId) {
       alert('Customer ID is missing. Please log in again.');
