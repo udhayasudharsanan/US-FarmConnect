ae3d38227c051d22595c6065231973c7a4d2bae3
udhayasudharsanan K <140906893+udhayasudharsanan@users.noreply.github.com>
2024-11-15 10:37:54 +0530

    COD and DELIVERY

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useCart } from '../context/CartContext';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import socket from '../socket'; // Assuming socket setup
 
 export default function Cart() {
